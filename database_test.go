3f2b0e8268ae0923263453e001c24a58fafcc9ab
석경우 <rudtyz@gmail.com>
2020-05-31 12:27:34 +0900

    db

---
+++ database_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	// sql
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func init() {
+	db, _ = sql.Open("mysql", "selectman:1234@tcp(127.0.0.1:3306)/my_database")
+}
+func TestDbSelect(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Nanosecond)
+	defer cancel()
+	fmt.Println(db)
+	rows, err := db.QueryContext(ctx, "select 1 as a")
+	log.Println(rows)
+	log.Println(err)
+}
