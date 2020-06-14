b3f2d3c1c3d6cfa0ba2d3a9d3744b1fced1e02a5
석경우 <rudtyz@gmail.com>
2020-06-14 11:36:36 +0900

    defaultMachineUniqueValue pid + random

--- generate_session_id_test.go
+++ generate_session_id_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"math/rand"
 	"os"
@@ -11,43 +10,25 @@ import (
 )
 
 var index uint32 = 998
-var machineUniqueValue []byte
+var machineUniqueValue string
 
 // NewSessionID 새로운 세션 아이디를 만듭니다
 func NewSessionID() string {
-	// b := make([]byte, 12+len(machineUniqueValue))
 	now := time.Now().Unix()
 	newIndex := atomic.AddUint32(&index, 1)
-	// binary.LittleEndian.PutUint64(b[:], uint64(now))
-	// binary.LittleEndian.PutUint32(b[8:], newIndex)
 
-	// copy(b[12:], machineUniqueValue)
-	// fmt.Println(b)
-	// fmt.Println(hex.EncodeToString(b))
-	// r := base64.StdEncoding.EncodeToString(b)
-	// fmt.Println(r)
-	// k, _ := base64.StdEncoding.DecodeString(r)
-	// fmt.Println(k)
-
-	return fmt.Sprintf("%d%03d%s",
-		now,
+	return fmt.Sprintf("%03d%d%s",
 		newIndex%1000,
-		defaultMachineUniqueValue2())
+		now,
+		machineUniqueValue)
 }
 
-func defaultMachineUniqueValue() []byte {
-	var b [12]byte
-	pid := uint64(os.Getpid())
-	randomValue := rand.Uint32()
-	binary.LittleEndian.PutUint64(b[:], pid)
-	binary.LittleEndian.PutUint32(b[8:], randomValue)
-	return b[:]
-}
-
-func defaultMachineUniqueValue2() string {
+// defaultMachineUniqueValue 각 서버별 고유 아이디
+// pid + 랜덤이면 소형 서버에서는 고유할것으로 생각
+func defaultMachineUniqueValue() string {
 	pid := os.Getpid()
-	randomValue := rand.Uint32() % 10
-	return fmt.Sprintf("%d%d", pid, randomValue)
+	randomValue := rand.Uint32() % 100
+	return fmt.Sprintf("%d%02d", pid, randomValue)
 }
 
 func init() {
