e5a4cf7ab32b6dcca657fc31ffe35c5913b829ff
석경우 <rudtyz@gmail.com>
2020-06-17 22:34:58 +0900

    return 1,2,3 value

--- cache/api_cache.go
+++ cache/api_cache.go
@@ -1,48 +1,80 @@
 package cache
 
 import (
+	"fmt"
 	"reflect"
 )
 
+type Any interface{}
+
 type leap struct {
-	value interface{}
-	next  map[interface{}]interface{}
+	value []reflect.Value
+	next  map[Any]Any
 }
 
 type FunctionCache struct {
-	leap     leap
-	Function interface{}
+	leap     *leap
+	Function Any
 }
 
-func NewFunctionCache(fn interface{}) *FunctionCache {
+func NewFunctionCache(fn Any) *FunctionCache {
 	f := &FunctionCache{}
 	f.Function = fn
-	f.leap.next = make(map[interface{}]interface{})
+	f.leap = &leap{
+		value: nil,
+		next:  make(map[Any]Any),
+	}
 	return f
 }
 
-func (f *FunctionCache) Call(args ...interface{}) interface{} {
-	a := f.leap
-	m := a.next
+func (f *FunctionCache) call(args ...Any) []reflect.Value {
+	cNode := f.leap
+	argMap := cNode.next
 	for i := 0; i < len(args); i++ {
-		value, ok := m[args[i]]
+		value, ok := argMap[args[i]]
 		if !ok {
 			newLeap := &leap{
 				value: nil,
-				next:  make(map[interface{}]interface{}),
+				next:  make(map[Any]Any),
 			}
-			m[args[i]] = newLeap
+			argMap[args[i]] = newLeap
 			value = newLeap
 		}
-		a = (value.(leap))
+		cNode = (value.(*leap))
 	}
 
-	if a.value == nil {
+	if cNode.value == nil {
 		fArgs := make([]reflect.Value, len(args))
 		for i := 0; i < len(args); i++ {
 			fArgs[i] = reflect.ValueOf(args[i])
 		}
-		a.value = reflect.ValueOf(f.Function).Call(fArgs)
+		cNode.value = reflect.ValueOf(f.Function).Call(fArgs)
 	}
-	return a.value
+	return cNode.value
+}
+
+func (f *FunctionCache) Call(args ...Any) Any {
+	ret := f.call(args...)
+	return ret[0].Interface()
+}
+
+func (f *FunctionCache) CallR2(args ...Any) (Any, Any) {
+	ret := f.call(args...)
+	if len(ret) != 2 {
+		panic(fmt.Sprint("function result length != 2. result: ", len(ret)))
+	}
+	r0 := ret[0].Interface()
+	r1 := ret[1].Interface()
+	return r0, r1
+}
+
+func (f *FunctionCache) CallR3(args ...Any) (Any, Any, Any) {
+	ret := f.call(args...)
+	if len(ret) != 3 {
+		panic(fmt.Sprint("function result length != 3. result: ", len(ret)))
+	}
+	r0 := ret[0].Interface()
+	r1 := ret[1].Interface()
+	r2 := ret[2].Interface()
+	return r0, r1, r2
 }
--- cache/api_cache_test.go
+++ cache/api_cache_test.go
@@ -5,10 +5,30 @@ import (
 	"testing"
 )
 
-func TestFunctionCache(t *testing.T) {
+func TestFunctionCacheArg0(t *testing.T) {
 	fc := NewFunctionCache(func() int {
 		return 3
 	})
 
 	fmt.Println(fc.Call())
+}
+
+func TestFunctionCacheArg1(t *testing.T) {
+	mustCallOnce := 0
+	fc := NewFunctionCache(func(a int) int {
+		mustCallOnce++
+		return a + 1
+	})
+
+	if fc.Call(1) != 2 {
+		t.Error("return 2")
+	}
+
+	if fc.Call(1) != 2 {
+		t.Error("return 2")
+	}
+
+	if mustCallOnce != 1 {
+		t.Error("cache error")
+	}
 }
