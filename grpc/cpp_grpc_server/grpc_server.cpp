e1cb2713f36d09861158d4f362360762d6e6b254
rudty <rudtyz@gmail.com>
2020-06-20 18:52:07 +0900

    cpp_server

---
+++ grpc/cpp_grpc_server/grpc_server.cpp
@@ -0,0 +1,72 @@
+﻿// vcpkg grpc
+// 
+// 
+// 복사 {vcpkgdir}\installed\x86-windows\tools\grpc .\bin\
+// .\bin\protoc --grpc_out=. "--plugin=protoc-gen-grpc=.\bin\grpc_cpp_plugin.exe" helloworld.proto
+// .\bin\protoc --cpp_out=. helloworld.proto
+//
+// 빌드 후 에러 발생 시
+// Windows.h include 순서가 맞지 않아서 생기는 문제
+// git clone grpc 해서 헤더를 최신 git 으로 교체 
+// 
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+
+#ifdef BAZEL_BUILD
+#include "examples/protos/helloworld.grpc.pb.h"
+#else
+#include "helloworld.grpc.pb.h"
+#endif
+#ifdef _MSC_VER
+#pragma comment(lib, "ws2_32.lib")
+#endif
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+using helloworld::Greeter;
+
+// Logic and data behind the server's behavior.
+class GreeterServiceImpl final : public Greeter::Service {
+    Status SayHello(ServerContext* context, const HelloRequest* request,
+        HelloReply* reply) override {
+        std::string prefix("Hello ");
+        std::cout << request->name() << std::endl;
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    GreeterServiceImpl service;
+
+    grpc::EnableDefaultHealthCheckService(true);
+    grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
+}
+
+int main(int argc, char** argv) {
+    RunServer();
+
+    return 0;
+}
