3bcbc92c5779c64762b0cf728ae033f2775a94f5
석경우 <rudtyz@gmail.com>
2020-06-23 19:08:21 +0900

    thread state

--- threading/thread.go
+++ threading/thread.go
@@ -1,22 +1,43 @@
 package threading
 
 import (
-	"fmt"
 	"reflect"
 	"runtime"
 	"sync"
+	"sync/atomic"
+)
+
+// ThreadStatus indicates the state of the thread
+type ThreadStatus int32
+
+const (
+	// NEW create thread
+	NEW ThreadStatus = iota
+
+	// STARTED thread is started. Start() function was called
+	STARTED
+
+	// TERMINATED thread is end
+	TERMINATED
 )
 
 // Thread OS thread
 type Thread struct {
-	wg sync.WaitGroup
-	fn interface{}
+	runPanic     interface{}
+	threadStatus int32
+	wg           sync.WaitGroup
+	fn           reflect.Value
 }
 
 // NewThread create thread with call function
 func NewThread(fn interface{}) *Thread {
+	r := reflect.ValueOf(fn)
+	if r.Kind() != reflect.Func {
+		panic("must function")
+	}
+
 	t := Thread{
-		fn: fn,
+		fn: r,
 	}
 	return &t
 }
@@ -24,7 +45,7 @@ func NewThread(fn interface{}) *Thread {
 func (t *Thread) onThreadPanic() {
 	r := recover()
 	if r != nil {
-		fmt.Println(r)
+		t.runPanic = r
 	}
 }
 
@@ -45,16 +66,30 @@ func (t *Thread) start(args []interface{}) {
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 
-	reflect.ValueOf(t.fn).Call(in)
+	defer atomic.StoreInt32(&t.threadStatus, int32(TERMINATED))
+
+	t.fn.Call(in)
 }
 
 // Start start os thread with argument
 func (t *Thread) Start(args ...interface{}) {
+	if t.threadStatus != int32(NEW) {
+		panic("thread already started")
+	}
+	atomic.StoreInt32(&t.threadStatus, int32(STARTED))
 	t.wg.Add(1)
 	go t.start(args)
 }
 
-// Join  blocks until end thread
+// Join blocks until end thread
 func (t *Thread) Join() {
 	t.wg.Wait()
+	if t.runPanic != nil {
+		panic(t.runPanic)
+	}
+}
+
+// GetStatus return state this thread
+func (t *Thread) GetStatus() ThreadStatus {
+	return ThreadStatus(t.threadStatus)
 }
--- threading/thread_test.go
+++ threading/thread_test.go
@@ -3,6 +3,7 @@ package threading
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestCreateThread(t *testing.T) {
@@ -11,4 +12,54 @@ func TestCreateThread(t *testing.T) {
 	})
 	a.Start()
 	a.Join()
+}
+
+func TestJoin(t *testing.T) {
+	end := false
+	a := NewThread(func() {
+		time.Sleep(100)
+		end = true
+	})
+	a.Start()
+	a.Join()
+
+	if !end {
+		t.Error("must end")
+	}
+}
+
+func TestStatus(t *testing.T) {
+	a := NewThread(func() {
+		time.Sleep(100)
+	})
+
+	if a.GetStatus() != NEW {
+		t.Error("status new")
+	}
+	a.Start()
+	if a.GetStatus() != STARTED {
+		t.Error("status start")
+	}
+	a.Join()
+	if a.GetStatus() != TERMINATED {
+		t.Error("status terminated")
+	}
+}
+
+func TestPanic(t *testing.T) {
+	a := NewThread(func() {
+		panic("?")
+	})
+
+	a.Start()
+	{
+		defer func() {
+			recover()
+		}()
+		a.Join()
+	}
+
+	if a.GetStatus() != TERMINATED {
+		t.Error("status terminated")
+	}
 }
