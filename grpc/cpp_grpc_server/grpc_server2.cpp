803f36a576ce44c7aa1d91f34dc9d3a44848398f
rudty <rudtyz@gmail.com>
2020-07-29 19:20:41 +0900

    수정3

--- grpc/cpp_grpc_server/grpc_server2.cpp
+++ grpc/cpp_grpc_server/grpc_server2.cpp
@@ -1,11 +1,26 @@
+// vcpkg grpc
+// 
+// 
+// 복사 {vcpkgdir}\installed\x86-windows\tools\grpc .\bin\
+// .\bin\protoc --grpc_out=. "--plugin=protoc-gen-grpc=.\bin\grpc_cpp_plugin.exe" helloworld.proto
+// .\bin\protoc --cpp_out=. helloworld.proto
+//
+// 빌드 후 에러 발생 시
+// Windows.h include 순서가 맞지 않아서 생기는 문제
+// git clone grpc 해서 헤더를 최신 git 으로 교체 
+// 
+#pragma warning (disable:26812)
+#pragma warning (disable:26495)
+
 #include <iostream>
 #include <memory>
-#include <string>
-
-#include <grpcpp/grpcpp.h>
-#include <grpc/support/log.h>
 #include <thread>
-
+#include <string>
+#include <ppltasks.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <future>
 #ifdef BAZEL_BUILD
 #include "examples/protos/helloworld.grpc.pb.h"
 #else
@@ -14,126 +29,159 @@
 #ifdef _MSC_VER
 #pragma comment(lib, "ws2_32.lib")
 #endif
-using grpc::Channel;
-using grpc::ClientAsyncResponseReader;
-using grpc::ClientContext;
-using grpc::CompletionQueue;
+using grpc::Server;
+using grpc::ServerAsyncResponseWriter;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerCompletionQueue;
 using grpc::Status;
 using helloworld::HelloRequest;
 using helloworld::HelloReply;
 using helloworld::Greeter;
 
-class GreeterClient {
+struct RpcRequestProcessable {
+    virtual ~RpcRequestProcessable() = default;
+    virtual void onProceed() = 0;
+};
+
+enum CallStatus { PROCESS, FINISH };
+
+
+template<
+    typename _Request, 
+    typename _Response>
+class MessageRpcRequest: public RpcRequestProcessable{
+    CallStatus status_ = PROCESS;
+    _Request req_;
+    _Response res_;
+    ServerContext ctx_;
+    ServerAsyncResponseWriter<_Response> responder_;
 public:
-    explicit GreeterClient(std::shared_ptr<Channel> channel)
-        : stub_(Greeter::NewStub(channel)) {}
 
-    // Assembles the client's payload and sends it to the server.
-    void SayHello(const std::string& user) {
-        // Data we are sending to the server.
-        HelloRequest request;
-        request.set_name(user);
+    MessageRpcRequest() : responder_(&ctx_) { std::cout << "MessageRpcRequest" << std::endl; }
 
-        // Call object to store rpc data
-        AsyncClientCall* call = new AsyncClientCall;
-
-        // stub_->PrepareAsyncSayHello() creates an RPC object, returning
-        // an instance to store in "call" but does not actually start the RPC
-        // Because we are using the asynchronous API, we need to hold on to
-        // the "call" instance in order to get updates on the ongoing RPC.
-        call->response_reader =
-            stub_->PrepareAsyncSayHello(&call->context, request, &cq_);
-
-        // StartCall initiates the RPC call
-        call->response_reader->StartCall();
-
-        // Request that, upon completion of the RPC, "reply" be updated with the
-        // server's response; "status" with the indication of whether the operation
-        // was successful. Tag the request with the memory address of the call object.
-        call->response_reader->Finish(&call->reply, &call->status, (void*)call);
-
+    template <typename _Service>
+    void registerServiceFunction(
+        _Service* svc, 
+        ServerCompletionQueue* cq, 
+        void(_Service::*f)(::grpc::ServerContext* context, _Request* request, ::grpc::ServerAsyncResponseWriter<_Response>* response, ::grpc::CompletionQueue* new_call_cq, ::grpc::ServerCompletionQueue* notification_cq, void* tag)) {
+        ((*svc).*f)(&ctx_, &req_, &responder_, cq, cq, this);
     }
 
-    // Loop while listening for completed responses.
-    // Prints out the response from the server.
-    void AsyncCompleteRpc() {
-        void* got_tag;
-        bool ok = false;
+    virtual void onCreate() = 0;
 
-         //Block until the next result is available in the completion queue "cq".
-        while (cq_.Next(&got_tag, &ok)) {
-            // The tag in this example is the memory location of the call object
-            AsyncClientCall* call = static_cast<AsyncClientCall*>(got_tag);
-
-            // Verify that the request was completed successfully. Note that "ok"
-            // corresponds solely to the request for updates introduced by Finish().
-            GPR_ASSERT(ok);
-
-            if (call->status.ok())
-                std::cout << "Greeter received: " << call->reply.message() << std::endl;
-            else
-                std::cout << "RPC failed" << call->status.error_code() << " " << call->status.error_details() << std::endl;
-
-            // Once we're complete, deallocate the call object.
-            delete call;
+    void onProceed() final override {
+        if (status_ == PROCESS) {
+            status_ = FINISH;
+            auto* newIam = onClone();
+            newIam->onCreate();
+            concurrency::create_task([this] {
+                try {
+                    onRequest(&req_, &res_);
+                    responder_.Finish(res_, Status::OK, this);
+                }
+                catch (std::invalid_argument& e) {
+                    responder_.FinishWithError(Status(grpc::StatusCode::INVALID_ARGUMENT, e.what()), this);
+                }
+                catch (std::exception& e) {
+                    responder_.FinishWithError(Status(grpc::StatusCode::INTERNAL, e.what()), this);
+                }
+            });
         }
+        else {
+            onRelease();
+        }
+    }
+
+    virtual void onRequest(_Request* req, _Response* res) = 0;
+    virtual MessageRpcRequest<_Request, _Response>* onClone() = 0;
+    virtual void onRelease() = 0;
+};
+
+template<typename _Request, typename _Response, typename _MyBase>
+class AbstractRequest: public MessageRpcRequest<_Request, _Response> {
+    _MyBase* onClone() override {
+        return new _MyBase(service_, cq_);
+    }
+    void onRelease() override {
+        delete this;
+    }
+};
+
+class SayHelloRequest: public AbstractRequest<HelloRequest, HelloReply, SayHelloRequest> {
+    Greeter::AsyncService* service_;
+    ServerCompletionQueue* cq_;
+public:
+    SayHelloRequest(Greeter::AsyncService* service, ServerCompletionQueue* cq)
+        : service_(service), cq_(cq) {
+        std::cout << "SayHelloRequest" << std::endl;
+    }
+
+    void onCreate() override {
+        registerServiceFunction(service_, cq_, &Greeter::AsyncService::RequestSayHello);
+    }
+
+    void onRequest(HelloRequest* req, HelloReply* res) override {
+        std::string prefix("Hello ");
+        res->set_message(prefix + req->name());
+        std::cout << "dd" << " " << GetCurrentThreadId() << std::endl;
+    }
+};
+
+class ServerImpl final: public Greeter::AsyncService {
+    using _MyBase = Greeter::AsyncService;
+    std::unique_ptr<ServerCompletionQueue> cq_;
+    std::unique_ptr<Server> server_;
+public:
+    ~ServerImpl() {
+        server_->Shutdown();
+        // Always shutdown the completion queue after the server.
+        cq_->Shutdown();
+    }
+
+    // There is no shutdown handling in this code.
+    void Run() {
+        std::string server_address("0.0.0.0:50051");
+
+        ServerBuilder builder;
+        // Listen on the given address without any authentication mechanism.
+        builder.AddListeningPort(server_address, grpc::InsecureServerCredentials()); 
+        // Register "service_" as the instance through which we'll communicate with
+        // clients. In this case it corresponds to an *asynchronous* service.
+        builder.RegisterService(this);
+        // Get hold of the completion queue used for the asynchronous communication
+        // with the gRPC runtime.
+        cq_ = builder.AddCompletionQueue();
+        // Finally assemble the server.
+        server_ = builder.BuildAndStart();
+        std::cout << "Server listening on " << server_address << std::endl;
+
+        // Spawn a new CallData instance to serve new clients.
+        (new SayHelloRequest(this, cq_.get()))->onCreate();
+        HandleRpcs();
     }
 
 private:
-
-    // struct for keeping state and data information
-    struct AsyncClientCall {
-        // Container for the data we expect from the server.
-        HelloReply reply;
-
-        // Context for the client. It could be used to convey extra information to
-        // the server and/or tweak certain RPC behaviors.
-        ClientContext context;
-
-        // Storage for the status of the RPC upon completion.
-        Status status;
-
-
-        std::unique_ptr<ClientAsyncResponseReader<HelloReply>> response_reader;
-
-        AsyncClientCall() {
-            std::chrono::system_clock::time_point deadline =
-                std::chrono::system_clock::now() + std::chrono::seconds(1000);
-            context.set_deadline(deadline);
+    // This can be run in multiple threads if needed.
+    void HandleRpcs() {
+        void* tag;  // uniquely identifies a request.
+        bool ok;
+        while (true) {
+            // Block waiting to read the next event from the completion queue. The
+            // event is uniquely identified by its tag, which in this case is the
+            // memory address of a CallData instance.
+            // The return value of Next should always be checked. This return value
+            // tells us whether there i     s any kind of event or cq_ is shutting down.
+            GPR_ASSERT(cq_->Next(&tag, &ok));
+         
+            static_cast<RpcRequestProcessable*>(tag)->onProceed();
         }
-    };
-
-    // Out of the passed in Channel comes the stub, stored here, our view of the
-    // server's exposed services.
-    std::unique_ptr<Greeter::Stub> stub_;
-
-    // The producer-consumer queue we use to communicate asynchronously with the
-    // gRPC runtime.
-    CompletionQueue cq_;
+    }
 };
 
 int main(int argc, char** argv) {
+    ServerImpl server;
+    server.Run();
 
-
-    // Instantiate the client. It requires a channel, out of which the actual RPCs
-    // are created. This channel models a connection to an endpoint (in this case,
-    // localhost at port 50051). We indicate that the channel isn't authenticated
-    // (use of InsecureChannelCredentials()).
-    GreeterClient greeter(grpc::CreateChannel(
-        "localhost:50051", grpc::InsecureChannelCredentials()));
-
-    // Spawn reader thread that loops indefinitely
-    std::thread thread_ = std::thread(&GreeterClient::AsyncCompleteRpc, &greeter);
-    while (true) {
-        for (int i = 0; i < 100; i++) {
-            std::string user("world " + std::to_string(i));
-            greeter.SayHello(user);  // The actual RPC call!
-        }
-        Sleep(1000);
-    }
-
-    std::cout << "Press control-c to quit" << std::endl << std::endl;
-    thread_.join();  //blocks forever
-    //getchar();
     return 0;
 }
